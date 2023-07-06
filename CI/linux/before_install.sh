fdd9a3b49c2a5e2057ad1234d7b55dbf2909e136
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-07-06 13:01:59 +0300

    fix: ci

--- CI/linux/before_install.sh
+++ CI/linux/before_install.sh
@@ -9,3 +9,5 @@ sudo apt-get install qtbase5-dev
 sudo apt-get install ninja-build zlib1g-dev libavformat-dev libswscale-dev libtbb-dev libluajit-5.1-dev
 # Optional dependencies
 sudo apt-get install libminizip-dev libfuzzylite-dev qttools5-dev
+# GTK 3
+sudo apt-get install libgtk-3-dev
